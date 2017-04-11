f67bc8f6764c8a162df44cbb599ebc3725279886
zlkca <zlk1979@hotmail.com>
2017-04-11 08:43:46 -0400

    Add login and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login',  component: LoginComponent },
+  { path: 'products/:id', component: ProductListComponent },
+  { path: 'signup',     component: SignupComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,19 +25,18 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     SignupComponent,
     HeaderComponent,
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     JsonpModule,
-    RouterModule.forRoot([{
-      path: 'products',
-      component: ProductListComponent
-    }]),
+    RouterModule.forRoot(routes),
     MaterialModule.forRoot()
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
